0c867d90fd58301a11e42ac5567e996f5d38b12c
skizoDUR <63934757+skizoDUR@users.noreply.github.com>
2022-01-12 09:43:33 -0300

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,14 @@
+//Example
+int f()
+{
+        restore = 0;
+        try {
+                //some code here
+        }
+        catch (...) {
+                       
+        }
+        finally (
+                        restore = 1;       
+                )
+}
