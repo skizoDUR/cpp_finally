f0246f20e53327a9f740513d1f3f0cf7474a051f
skizoDUR <63934757+skizoDUR@users.noreply.github.com>
2022-01-13 14:48:25 -0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -2,13 +2,9 @@
 void f()
 {
         restore = 0;
-        try {
-                //some code here
-        }
-        catch (...) {
-                       
-        }
+        
         finally (
                         restore = 1;       
                 )
+        this_function_can_throw();
 }
